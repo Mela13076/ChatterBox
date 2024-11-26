5e6a85daf8d3b39a727629b2b8169bcfe7a34538
dumpatipallavi <dumpatipallavi@gmail.com>
2024-11-25 23:37:34 -0600

    added UI screens

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import logo from "../assets/Logo.png";
+import React from 'react'
+import logo from '../assets/Logo.png'
 
-const AuthLayouts = ({ children }) => {
+const AuthLayouts = ({children}) => {
   return (
-    <div>
-      <header className="flex justify-center items-center py-3 shadow-md">
-        <img src={logo} alt="Logo" width={180} height={60} />
-      </header>
-      {children}
-    </div>
-  );
-};
+    <>
+        <header className='flex justify-center items-center py-3 h-20 shadow-md bg-white'>
+            <img 
+              src={logo}
+              alt='logo'
+              width={180}
+              height={60}
+            />
+        </header>
 
-export default AuthLayouts;
+        { children }
+    </>
+  )
+}
+
+export default AuthLayouts
